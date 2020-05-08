b885805520b95d9fd3b25e9f4ebbd0776f50c4e3
huynghiem99 <huynghiem99@gmail.com>
2020-05-08 18:03:28 +0700

    first

---
+++ Thêm sách vào tủ,search sách/Model/Book.swift
@@ -0,0 +1,9 @@
+//
+//  Book.swift
+//  Thêm sách vào tủ,search sách
+//
+//  Created by macOS on 5/8/20.
+//  Copyright © 2020 macOS. All rights reserved.
+//
+
+import Foundation
---
+++ Thêm sách vào tủ,search sách/View/TableViewCellBook.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellBook.swift
+//  Thêm sách vào tủ,search sách
+//
+//  Created by macOS on 5/8/20.
+//  Copyright © 2020 macOS. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellBook: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
